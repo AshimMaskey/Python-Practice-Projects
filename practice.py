c37d9d834f2426e6a8d8293db68bc43f2c35902f
Ashim Maskey <ashimmaskey4@gmail.com>
2025-05-01 11:07:45 +0545

    tip calculator

---
+++ practice.py
@@ -0,0 +1,7 @@
+age=int(input("what is your age? "))
+remaining_age=90-age
+remanining_days=remaining_age*365
+remaining_weeks=remaining_age*52
+remanining_months=remaining_age*12
+
+print(f"You have {remanining_days} days, {remaining_weeks} weeks, {remanining_months} months left")
