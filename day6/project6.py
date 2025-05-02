eafffcd57db6df8f61756040fc945fa216963faf
Ashim Maskey <ashimmaskey4@gmail.com>
2025-05-02 18:20:18 +0545

    added about function

---
+++ day6/project6.py
@@ -0,0 +1,8 @@
+# functions
+def hello():
+	print("hello")
+hello()
+
+def param(name):
+	print(f"My name is {name}")
+param("ashim")
--- practice.py
+++ practice.py
@@ -1,9 +1,5 @@
-for i in range(1,100):
-	if i%3==0 and i%5==0:
-		print("fizzbuzz")
-	elif i%3==0:
-		print("fizz")
-	elif i%5==0:
-		print("buzz")
-	else:
-		print(i)
+value=0
+
+while value<5:
+	print("hello")
+	value+=1
